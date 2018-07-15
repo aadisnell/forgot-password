4425c044fa76376d0d5978b2a2deb39d6b1899db
Asirifi charles <asirificharles@yahoo.com>
2018-07-15 13:59:54 -0700

    password reset

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '@snELL0541');
+define('DB_DATABASE', 'online');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
